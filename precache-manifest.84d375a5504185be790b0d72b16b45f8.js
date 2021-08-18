525ff1a85f2d0f8949abd84171bc991f3da0f67d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-08-18 08:18:03 +0000

    Deploy HSQCoollaughing/Coollaughing-StoryBook to github.com/HSQCoollaughing/Coollaughing-StoryBook.git:gh-pages

---
+++ precache-manifest.84d375a5504185be790b0d72b16b45f8.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4750f852b420502df9b2916325379aa4",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a5c153d30f59dbc9bd39.bundle.js"
+  },
+  {
+    "revision": "e9fbc6c91194a2993f16",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.a5c153d30f59dbc9bd39.bundle.js"
+  },
+  {
+    "url": "vendors~main.a5c153d30f59dbc9bd39.bundle.js"
+  },
+  {
+    "revision": "3a9a49ed3d8d4a0a5614",
+    "url": "vendors~main.css"
+  }
+]);

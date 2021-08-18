c3a1238527eed73d5ca74fa887b58352b2c172b6
HSQ <1070404612@qq.com>
2021-08-18 16:08:15 +0800

    1

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -5,12 +5,12 @@ storiesOf('Welcome page', module)
   .add('welcome', () => {
     return (
       <>
-        <h1>欢迎来到 vikingship 组件库</h1>
-        <p>vikingship 是为慕课网课程打造的一套教学组件库，从零到一让大家去学习</p>
-        <h3>安装试试</h3>
+        <h1>欢迎来到 Coollaughing 组件库</h1>
+
         <code>
-          npm install vikingship --save
+          {/* npm install vikingship --save */}
+          npm install
         </code>
       </>
     )
-  }, { info : { disable: true }})
+  }, { info: { disable: true } })
